5bd7b2fbdf556dffa62b89d1c4de6b1e92eef601
magnus-sg <weesiang@outlook.com>
2015-07-14 22:37:24 +0800

    First Commit

--- Camera1/ViewController.swift
+++ Camera1/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
 
-
+    /*
+var imageView = UIImageView()
+    
+    @IBOutlet weak var imageViewContainer: UIView! {
+        
+        didSet {
+            imageViewContainer.addSubview(imageView)
+        
+        }
+    }
+  
+*/
+    
+    @IBAction func takePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .Camera
+            
+            picker.mediaTypes = [kUTTypeImage]
+            picker.delegate = self
+            picker.allowsEditing = true
+            presentViewController(picker, animated: true, completion: nil)
+            
+            
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        var image = info[UIImagePickerControllerEditedImage] as? UIImage
+        if image == nil {
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        imageView.image = image
+        //makeRoomForImage()
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 }
 
